e64a04ba103934ec48c8332b623ba18accbfa0dc
MhmdAL <mohammadalnoss@outlook.com>
2022-02-03 07:04:35 +0300

    more s ripts

--- app/test2.py
+++ app/test2.py
@@ -1,8 +1,9 @@
 import olympe
 import os
-from olympe.messages.ardrone3.Piloting import TakeOff, moveBy, Landing
-from olympe.messages.ardrone3.PilotingState import FlyingStateChanged
+from olympe.messages.ardrone3.Piloting import TakeOff, moveBy, moveTo, Landing
+from olympe.messages.ardrone3.PilotingState import moveToChanged, FlyingStateChanged
 from olympe.messages.ardrone3.PilotingEvent import moveByEnd
+from olympe.messages.common.Mavlink import Start
 
 DRONE_IP = os.environ.get("DRONE_IP", "10.202.0.1")
 
@@ -36,23 +37,13 @@ def test_moveby2():
     drone.connect()
 
     assert drone(
-        TakeOff(_timeout=15)
-        >> FlyingStateChanged(state="hovering")
-    ).wait().success()
-
-    assert drone(
-        moveBy(1, 1, 0, 0, _timeout=100)
-        >> FlyingStateChanged(state="hovering")
-        >> moveBy(-1, -1, 0, 0, _timeout=100)
-        >> FlyingStateChanged(state="hovering")
-    ).wait().success()
-
-    assert drone(
-        Landing(_timeout=15)
+        Start("ab1cd1f9ae04bfbc0c469e2e9ee3fd4b", 'flightPlan', _timeout=10000)
+        >> FlyingStateChanged(state="landing")
         >> FlyingStateChanged(state="landed")
     ).wait().success()
 
     drone.disconnect()
+
 
 
 if __name__ == "__main__":
