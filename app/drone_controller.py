babba927a0a4c7b627521cec7dad572c5f651cd9
MhmdAL <mohammadalnoss@outlook.com>
2022-03-02 00:56:59 +0300

    mc

--- app/drone_controller.py
+++ app/drone_controller.py
@@ -184,7 +184,7 @@ def print_event(event):
 
 class FlightListener(olympe.EventListener):
 
-    @olympe.listen_event(MavlinkFilePlayingStateChanged())
+    @olympe.listen_event(MavlinkFilePlayingStateChanged(_policy = 'wait'))
     def onMavlinkFilePlayingStateChanged(self, event, scheduler):
         print_event(event)
         on_flight_mission_completed()
